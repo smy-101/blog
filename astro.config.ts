d0c84cd7530144293b4ee9a7a93530321c37b080
smy-101 <shimy1011@foxmail.com>
2024-09-01 19:39:43 +0800

    测试 Github Action

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: THEME_CONFIG.website,
-  base: '/blog_dist',
+  base: '/blog',
   prefetch: true,
   markdown: {
     shikiConfig: {
